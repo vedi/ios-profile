b1ed51fe72c2109fb4e3c0eb79edf6cc8c2977c6
lassic <homedev@gmail.com>
2014-06-24 16:31:55 +0300

    fix warnings

--- SoomlaiOSProfileExample/SoomlaiOSProfileExample/ViewController.m
+++ SoomlaiOSProfileExample/SoomlaiOSProfileExample/ViewController.m
@@ -14,6 +14,7 @@
 #import "UserProfileEventHandling.h"
 #import "StoreEventHandling.h"
 #import "SoomlaUtils.h"
+#import "VirtualItemReward.h" // to avoid "incompatible type" warnings
 
 
 @implementation ViewController
