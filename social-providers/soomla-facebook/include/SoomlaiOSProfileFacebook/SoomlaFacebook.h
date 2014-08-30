cc979521cd0a20770a217f73365f29f28cc63cb2
sluzky <simon@soom.la>
2014-08-30 15:05:52 +0300

    updating soomla facebook ios bin

--- social-providers/soomla-facebook/include/SoomlaiOSProfileFacebook/SoomlaFacebook.h
+++ social-providers/soomla-facebook/include/SoomlaiOSProfileFacebook/SoomlaFacebook.h
@@ -18,6 +18,8 @@
 
 #import <FacebookSDK/FacebookSDK.h>
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedClassInspection"
 /**
  This class creates a plugin for *ios-store* using Facebook as its social provider
  With this plugin, you can use our pre-cooked social actions (login, share status etc.)
@@ -30,26 +32,12 @@
     loginFail loginFail;
     loginCancel loginCancel;
     logoutSuccess logoutSuccess;
-    logoutFail logoutFail;
-    userProfileSuccess userProfileSuccess;
-    userProfileFail userProfileFail;
-    socialActionSuccess socialActionSuccess;
-    socialActionFail socialActionFail;
-    feedsActionSuccess feedsActionSuccess;
-    feedsActionFail feedsActionFail;
 }
 
 @property (strong, nonatomic) loginSuccess loginSuccess;
 @property (strong, nonatomic) loginFail loginFail;
 @property (strong, nonatomic) loginCancel loginCancel;
 @property (strong, nonatomic) logoutSuccess logoutSuccess;
-@property (strong, nonatomic) logoutFail logoutFail;
-@property (strong, nonatomic) userProfileSuccess userProfileSuccess;
-@property (strong, nonatomic) userProfileFail userProfileFail;
-@property (strong, nonatomic) socialActionSuccess socialActionSuccess;
-@property (strong, nonatomic) socialActionFail socialActionFail;
-@property (strong, nonatomic) feedsActionSuccess feedsActionSuccess;
-@property (strong, nonatomic) feedsActionFail feedsActionFail;
 
 // Make this method public to allow access from App Delegate
 
@@ -66,3 +54,5 @@
 - (void)sessionStateChanged:(FBSession *)session state:(FBSessionState) state error:(NSError *)error;
 
 @end
+
+#pragma clang diagnostic pop
