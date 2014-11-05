8213056fb52cdddc265489271aab788e0b8b7d4a
Dima Nemets <dima@soom.la>
2014-11-05 19:27:59 +0200

    Created initial GooglePlus provider project

---
+++ social-providers/ios-profile-google/SoomlaiOSProfileGoogle/SoomlaiOSProfileGoogle/SoomlaiOSProfileGoogle.h
@@ -0,0 +1,13 @@
+//
+//  SoomlaiOSProfileGoogle.h
+//  SoomlaiOSProfileGoogle
+//
+//  Created by Dima on 11/5/14.
+//  Copyright (c) 2014 SOOMLA Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoomlaiOSProfileGoogle : NSObject
+
+@end
---
+++ social-providers/ios-profile-google/SoomlaiOSProfileGoogle/SoomlaiOSProfileGoogle/SoomlaiOSProfileGoogle.m
@@ -0,0 +1,13 @@
+//
+//  SoomlaiOSProfileGoogle.m
+//  SoomlaiOSProfileGoogle
+//
+//  Created by Dima on 11/5/14.
+//  Copyright (c) 2014 SOOMLA Inc. All rights reserved.
+//
+
+#import "SoomlaiOSProfileGoogle.h"
+
+@implementation SoomlaiOSProfileGoogle
+
+@end
