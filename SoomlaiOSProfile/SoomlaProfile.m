30dbb708073f83a9afae469c1912cf0e82e23356
Omri Masri <omri@soom.la>
2015-06-17 17:41:54 +0300

    cosmetics

--- SoomlaiOSProfile/SoomlaProfile.m
+++ SoomlaiOSProfile/SoomlaProfile.m
@@ -30,8 +30,6 @@ BOOL UsingExternalProvider;
 
 @implementation SoomlaProfile
 
-
-
 + (void)usingExternalProvider:(BOOL)isExternal {
     
     UsingExternalProvider = isExternal;
@@ -175,8 +173,6 @@ BOOL UsingExternalProvider;
                              andCustomMessage: customMessage];
 }
 
-
-
 - (void)updateStoryWithProvider:(Provider)provider
                      andMessage:(NSString *)message
                         andName:(NSString *)name
@@ -231,7 +227,6 @@ BOOL UsingExternalProvider;
     [socialController uploadImageWithProvider:provider andMessage:message andImageFileName:fileName andImageData:imageData andPayload:payload andReward:reward andShowConfirmation: false];
 }
 
-
 - (void)uploadImageWithProvider:(Provider)provider andMessage:(NSString *)message andFilePath:(NSString *)filePath andPayload:(NSString *)payload andReward:(Reward *)reward andConfirmation:(BOOL)showConfirmation andCustomMessage:(NSString *)customMessage {
 
     [socialController uploadImageWithProvider:provider
@@ -242,8 +237,6 @@ BOOL UsingExternalProvider;
                           andShowConfirmation:showConfirmation
                              andCustomMessage:customMessage];
 }
-
-
 
 - (void)uploadImageWithProvider:(Provider)provider
                      andMessage:(NSString *)message
@@ -282,11 +275,11 @@ BOOL UsingExternalProvider;
     float iOSVersion = [[UIDevice currentDevice].systemVersion floatValue];
     NSString *reviewURL;
     
-    if (iOSVersion<7) { //IOS version is less than 7
+    if (iOSVersion<7) { //IOS < 7.0
         NSString *templateReviewURLiOS6 = @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=APP_ID";
         reviewURL = [templateReviewURLiOS6 stringByReplacingOccurrencesOfString:@"APP_ID" withString:appID];
         
-    } else { //IOS version is 7 or more
+    } else { //IOS >= 7.0
         NSString *templateReviewURLiOS7 = @"itms-apps://itunes.apple.com/app/idAPP_ID";
         reviewURL = [templateReviewURLiOS7 stringByReplacingOccurrencesOfString:@"APP_ID" withString:[NSString stringWithFormat:@"%@", appID]];
     }
@@ -295,7 +288,6 @@ BOOL UsingExternalProvider;
     [ProfileEventHandling postUserRating];
     
 }
-
 
 - (void)multiShareWithText:(NSString *)text andImageFilePath:(NSString *)imageFilePath {
     NSArray *postItems;
